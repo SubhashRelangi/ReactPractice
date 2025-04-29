3315df99a62bf0910654c2bc1f263dc53a70e4d1
SubhashRelangi <subhashrelangi@gmail.com>
2025-04-29 13:10:49 +0530

    16tut completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./header";
+import Content from "./content";
+import Footer from "./footer";
+import Home from "./home";
+import Newpost from "./newpost";
+import Postpage from "./postpage";
+import About from "./about";
+import Missing from "./missing";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Content />
+      <Routes>
+        {/* switch is the v6 keyword in react its not working use routes instead of switch */}
+        <Route path="/" element={<Home />} />
+        <Route path="/post" element={<Newpost />} />
+        <Route path="/post/:id" element={<Postpage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Missing />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
