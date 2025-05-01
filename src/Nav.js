cace5ee3b7afac32c64b3b5a7fdc63df62ae1bfa
SubhashRelangi <subhashrelangi@gmail.com>
2025-05-01 11:03:24 +0530

    21tut completed

--- src/Nav.js
+++ src/Nav.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import DataContext from './context/dataContext';
+import { useContext } from 'react'
 
-const Nav = ({search, setSearch}) => {
+const Nav = () => {
+
+  const { search, setSearch } = useContext(DataContext);
+  
+
   return (
     <main className='Nav'>
         <form className='searchForm' onSubmit={(e) => e.preventDefault()}>
